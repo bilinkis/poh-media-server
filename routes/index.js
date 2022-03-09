7edd5d577e4342862f188339a1ded757737e4096
Nico Bilinkis <nico@bilinkis.com>
2022-03-09 12:59:03 -0300

    added / for vercel

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,9 @@ var router = express.Router();
 let {videoSanitizer, photoSanitizer} = require('../controllers/mediaController')
 
 /* GET home page. */
+router.get('/',function(req,res){
+    res.send("Hello")
+})
 router.post('/video', videoSanitizer);
 router.post('/photo', photoSanitizer);
 
